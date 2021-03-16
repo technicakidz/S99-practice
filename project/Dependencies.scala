e2a75e8e5c88fe18646880a74b04f591eaf11390
technicakidz <y.technicakidz08@gmail.com>
2021-03-16 23:32:03 +0900

    test passed!

--- project/Dependencies.scala
+++
@@ -1,5 +0,0 @@
-import sbt._
-
-object Dependencies {
-  lazy val scalaTest = "org.scalacheck" %% "scalacheck" % "3.2.2"
-}
--- src/test/scala/example/HelloSpec.scala
+++ src/test/scala/example/HelloSpec.scala
@@ -1,10 +1,10 @@
-package example
-
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers
-
-class HelloSpec extends AnyFlatSpec with Matchers {
-  "The Hello object" should "say hello" in {
-    Hello.greeting shouldEqual "hello"
-  }
-}
+//package example
+//
+//import org.scalatest.Matchers
+//import org.scalatest.Flatspec.AnyFlatSpec
+//
+//class HelloSpec extends AnyFlatSpec with Matchers {
+//  "The Hello object" should "say hello" in {
+//    Hello.greeting shouldEqual "hello"
+//  }
+//}
