167de9ba99fbea98319c6294fc6b242862a9491e
technicakidz <y.technicakidz08@gmail.com>
2021-03-17 00:04:12 +0900

    test passed!

--- src/main/scala/example/P02.scala
+++ src/main/scala/example/P02.scala
@@ -1,5 +1,16 @@
 package example
 
-class P02 {
+/**
+ * P02 (*) Find the last but one element of a list.
+ * Example:
+ * scala> penultimate(List(1, 1, 2, 3, 5, 8))
+ * res0: Int = 5
+ */
 
+object P02 {
+  def penultimate[T](list: List[T]):T = list match {
+    case x :: _ :: Nil => x
+    case _ :: y :: xs  => penultimate(y :: xs)
+    case _        => sys.error("指定される条件の文字列がないかリストが空です")
+  }
 }
