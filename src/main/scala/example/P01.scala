2011f27a14afdcc3b99dc43ca4df21e31dddd3f2
technicakidz <y.technicakidz08@gmail.com>
2021-04-21 00:24:44 +0900

    P07

--- src/main/scala/example/P01.scala
+++ src/main/scala/example/P01.scala
@@ -1,7 +1,7 @@
 package example
 
 object P01 {
-  def last[T](list: List[T]): T = list match { // type parameter is
+  def last[T](list: List[T]): T = list match {
     case x :: Nil => x
     case x :: xs  => last(xs)
     case _        => sys.error("last isn't defined for empty list")
