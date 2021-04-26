8142928479f10c1590972cbd7e80bb3e475e3a90
technicakidz <y.technicakidz08@gmail.com>
2021-04-26 23:53:35 +0900

    passed

--- src/main/scala/example/P09.scala
+++ src/main/scala/example/P09.scala
@@ -9,9 +9,38 @@ package example
  */
 
 object P09 {
-  def pack[T](list: List[T]):List[List[T]] = {
-    list.foldRight(???)
+  def pack[T](ls:List[T]):List[List[T]] = {
+    def packA(a: List[T], b: List[T]): List[List[T]] = b match {
+      case x :: xs if a.head == x => packA(x :: a, xs)
+      case x :: xs => a :: packA(List(x), xs)
+      case _ => a :: Nil
+    }
+    ls match {
+      case x :: xs => packA(List(x), xs)
+      case _ => Nil
+    }
   }
 }
 
+
+// 模範解答
+// https://github.com/scala-text/S99/blob/master/src/main/scala/jp/co/dwango/s99/P09.scala
+//def pack[T](list: List[T]):List[List[T]] = {
+//  list.foldRight(Nil: List[List[T]]) {
+//  (s, List) => list match {
+//  case (x @ `e` :: _) :: xs => (s :: x) :: xs
+//  case _ => List(s) :: List
+//  }
+//  }
+//  }
+
 // example: foldRight
+// Listの全ての要素を足し合わせるメソッドsumをfoldRightを用いて実装してみましょう。
+// sumの宣言は次のようになります。なお、Listが空のときは0を返してみましょう。
+// https://namu-r21.hatenablog.com/entry/2019/07/21/211121
+//def sum(list: List[Int]): Int = list match {
+//  case x :: xs => list.foldRight[Int](0)((x, y) => x+y)
+//  case _ => 0
+//  }
+// または
+// def sum(list: List[Int]): Int = list.foldRight(0){(x, y) => x + y}
