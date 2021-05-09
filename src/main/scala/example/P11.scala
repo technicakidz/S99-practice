ad378154a50fec0eb329a61d4f35da4c52d58bb7
technicakidz <y.technicakidz08@gmail.com>
2021-05-09 23:23:22 +0900

    P11

---
+++ src/main/scala/example/P11.scala
@@ -0,0 +1,24 @@
+package example
+
+import Util.map
+import P09.pack
+import P04.length
+
+/**
+ * Modified run-length encoding.
+ * Modify the result of problem P10 in such a way that if an element has no duplicates it is simply copied into the result list. Only elements with duplicates are transferred as (N, E) terms.
+ * Example:
+ * scala> encodeModified(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+ * res0: List[Any] = List((4,'a), 'b, (2,'c), (2,'a), 'd, (4,'e))
+ */
+
+object P11 {
+  def encodeModified(list:List[T]):List[T] = {
+    map(pack(list)){x =>
+      length(x) match {
+        case 1 => x.head
+        case _ => (i, x.head)
+      }
+    }
+  }
+}
