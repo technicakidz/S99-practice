d4c8fa2bef110a2963a272a6981efb35d2025172
technicakidz <y.technicakidz08@gmail.com>
2021-05-19 23:31:04 +0900

    solved

--- src/main/scala/example/P15.scala
+++ src/main/scala/example/P15.scala
@@ -1,5 +1,11 @@
 package example
 
+import Util.flatMap
+import Util.copyN
+
 object P15 {
-  def duplicateN() = {}
+  def duplicateN[T](n: Int, list: List[T]): List[T] = {
+    flatMap(list) {case x => copyN(n, x)}
+    }
+  }
 }
