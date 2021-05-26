484c394b558206c9bf99a70d26f73bec8f7204e9
technicakidz <y.technicakidz08@gmail.com>
2021-05-26 23:31:45 +0900

    fixed

--- src/main/scala/jp/co/example/P16.scala
+++ src/main/scala/jp/co/example/P16.scala
@@ -1,21 +1,17 @@
 package jp.co.example
 
 object P16 {
-<<<<<<< HEAD
-  def drop[T](n:Int, list:List[T]):List[T] = {
-    def dropA(m:Int,list:List[T]):List[T] = list match {
-      case x :: xs => if m == 1 dropA(n, xs)
-      case x :: xs => if m > 1 x :: dropA(m -1, xs)
+  def drop[T](n: Int, list:List[T]):List[T] = {
+    def dropA(m: Int, list:List[T]):List[T] = list match {
+      case x :: xs if m == 1 => dropA(n, xs)
+      case x :: xs if m > 1 => x :: dropA(m -1, xs)
       case _ => List[T]() // ここで対象の要素を unit で返し drop する
     }
     if (n > 0) dropA(n, list) else sys.error("error")
+  }
+}
 //    if val tagret:List[Int] = list(n)
 //    else sys.error("nothing element")
 //    list match {
 //      case x :: xs => ???
 //    }
-  }
-=======
-  def hoge() = ???
->>>>>>> 7d0188166d2ae8f974fd6c345765867375e793b6
-}
