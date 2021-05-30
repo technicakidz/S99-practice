a941ddf9f90422d4afb36b8b6a94bb4d0990fc46
technicakidz <y.technicakidz08@gmail.com>
2021-05-31 00:26:14 +0900

    solved

--- src/main/scala/jp/co/example/P17.scala
+++ src/main/scala/jp/co/example/P17.scala
@@ -1,7 +1,14 @@
 package jp.co.example
 
-object P17{
-  def split[T](n: Int, list:List[T]): (List[T], List[T]) = {
-    ???
-  }
+object P17 {
+  def split[T](n: Int, list:List[T]): (List[T], List[T]) =
+    list match {
+        // 0 のときリストはそのまま返す
+        // n = 1 のとき一つ目の要素を xs で返す
+        // n = 2以上のとき先頭要素に n-1 目の要素を連結して返す
+      case x :: xs if n == 0 => (List[T](), xs)
+      case x :: xs if n > 0 =>
+        split(n - 1, xs) match { case (a, b) => (x :: a, b) }
+      case _ => sys.error("")
+    }
 }
